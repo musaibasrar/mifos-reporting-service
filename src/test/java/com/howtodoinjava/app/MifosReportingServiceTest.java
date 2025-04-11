3ee006dad8ca29a69339a9955819fef7bc7e4438
Uzma Tazeen <tazeen259@gmail.com>
2025-04-11 23:35:45 +0530

    Add Loans Pending Approval Jasper Report with SQL query

--- src/test/java/com/howtodoinjava/app/MifosReportingServiceTest.java
+++ src/test/java/com/howtodoinjava/app/MifosReportingServiceTest.java
@@ -1,15 +1,16 @@
 package com.howtodoinjava.app;
 
+import org.ideoholic.mrs.MifosReportingService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MifosReportingService.class)
 class MifosReportingServiceTest {
 
-	@Test
-	@Disabled
-	void contextLoads() {
-	}
-
+    @Test
+    @Disabled
+    void contextLoads() {
+    }
 }
+
