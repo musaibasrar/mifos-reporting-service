6ea1f6ea8c57fbeb57e982b4ac5266c5eb592665
Reehan <reehan.lalkhan@gmail.com>
2025-04-17 23:21:28 +0530

    Create Report Controller to generate Jasper reports in PDF format

--- src/main/java/org/ideoholic/mrs/MifosReportingService.java
+++ src/main/java/org/ideoholic/mrs/MifosReportingService.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Springdoc OpenAPI documentation: http://localhost:8080/swagger-ui.html
  * OpenAPI specification (in JSON format): http://localhost:8080/v3/api-docs
+ * To access H2 database: http://localhost:8080/h2-console
  */
 @SpringBootApplication
 public class MifosReportingService implements CommandLineRunner {
--- src/main/java/org/ideoholic/mrs/jasper/JasperReportService.java
+++ src/main/java/org/ideoholic/mrs/jasper/JasperReportService.java
@@ -1,15 +1,35 @@
 package org.ideoholic.mrs.jasper;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import javax.sql.DataSource;
+
+import org.ideoholic.mrs.dao.ReportParamRelationRepository;
+import org.ideoholic.mrs.dao.ReportParamRepository;
+import org.ideoholic.mrs.dao.ReportRepository;
+import org.ideoholic.mrs.dto.ReportGenParamDto;
+import org.ideoholic.mrs.dto.ReportGenerationParamsDto;
+import org.ideoholic.mrs.dto.ReportParamDto;
+import org.ideoholic.mrs.dto.ReportParamDtoList;
+import org.ideoholic.mrs.mappers.ReportParamMapper;
 import org.ideoholic.mrs.model.Item;
+import org.ideoholic.mrs.model.Report;
+import org.ideoholic.mrs.model.ReportParam;
+import org.ideoholic.mrs.model.ReportParamRelation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
@@ -19,78 +39,179 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.engine.util.JRSaver;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 
 @Slf4j
 @Service
 public class JasperReportService {
 
-  public byte[] getItemReport(List<Item> items, String format) {
+	@Autowired
+	@Qualifier("reportDataSource")
+	private DataSource reportDataSource;
 
-    JasperReport jasperReport;
+	@Autowired
+	private ResourceLoader resourceLoader;
 
-    try {
-      jasperReport = (JasperReport)
-          JRLoader.loadObject(ResourceUtils.getFile("item-report.jasper"));
-    } catch (FileNotFoundException | JRException e) {
-      try {
-        File file = ResourceUtils.getFile("classpath:item-report.jrxml");
-        jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRSaver.saveObject(jasperReport, "item-report.jasper");
-      } catch (FileNotFoundException | JRException ex) {
-        throw new RuntimeException(e);
-      }
-    }
+	@Autowired
+	private ReportParamMapper reportParamMapper;
 
-    JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(items);
-    Map<String, Object> parameters = new HashMap<>();
-    parameters.put("title", "Item Report");
-    JasperPrint jasperPrint = null;
-    byte[] reportContent;
+	@Autowired
+	private ReportRepository reportRepository;
 
-    try {
-      jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-      switch (format) {
-        case "pdf" -> reportContent = JasperExportManager.exportReportToPdf(jasperPrint);
-        case "xml" -> reportContent = JasperExportManager.exportReportToXml(jasperPrint).getBytes();
-        case "xls" -> reportContent = exportToXls(jasperPrint);
-        default -> throw new RuntimeException("Unknown report format");
-      }
-    } catch (JRException e) {
-      throw new RuntimeException(e);
-    }
-    return reportContent;
-  }
-  
-	public byte[] exportToXls(JasperPrint jasperPrint) {
+	@Autowired
+	private ReportParamRepository reportParamRepo;
+
+	@Autowired
+	private ReportParamRelationRepository reportParamRelationRepo;
+
+	public byte[] getItemReport(List<Item> items, String format) {
+
+		JasperReport jasperReport;
+
 		try {
-			// Create a ByteArrayOutputStream to hold the Excel data in memory
-			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			jasperReport = (JasperReport) JRLoader.loadObject(ResourceUtils.getFile("item-report.jasper"));
+		} catch (FileNotFoundException | JRException e) {
+			try {
+				File file = ResourceUtils.getFile("classpath:item-report.jrxml");
+				jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+				JRSaver.saveObject(jasperReport, "item-report.jasper");
+			} catch (FileNotFoundException | JRException ex) {
+				throw new RuntimeException(e);
+			}
+		}
 
-			// Initialize the XLS exporter
-			JRXlsExporter exporter = new JRXlsExporter();
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(items);
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("title", "Item Report");
+		JasperPrint jasperPrint = null;
+		byte[] reportContent;
 
-			// Set export parameters
-			exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-			exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(byteArrayOutputStream));
+		try {
+			jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+			switch (format) {
+			case "pdf" -> reportContent = JasperExportManager.exportReportToPdf(jasperPrint);
+			case "xml" -> reportContent = JasperExportManager.exportReportToXml(jasperPrint).getBytes();
+			case "xls" -> reportContent = exportToXls(jasperPrint);
+			default -> throw new RuntimeException("Unknown report format");
+			}
+		} catch (JRException e) {
+			throw new RuntimeException(e);
+		}
+		return reportContent;
+	}
 
-			// Export the report to XLS format
-			exporter.exportReport();
+	public byte[] generateReport(ReportGenerationParamsDto reportGenerationParamsDto, String reportId,
+			String fileFormat) {
+		String jrxmlPath = "classpath:";
+		Map<String, Object> parameters = new HashMap<>();
+		Map<String, String> userParameters = reportGenerationParamsDto.getReportParams().stream()
+				.collect(Collectors.toMap(ReportGenParamDto::getParamId, ReportGenParamDto::getParamValue));
 
-			log.debug("Report exported to XLS successfully!");
-
-			// Convert the ByteArrayOutputStream to a byte array
-			byte[] xlsData = byteArrayOutputStream.toByteArray();
-
-			return xlsData;
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		Report report = reportRepository.findByReportId(reportId).orElse(null);
+		if (report != null) {
+			jrxmlPath += report.getReportFile();
+			List<ReportParamRelation> relations = reportParamRelationRepo.findAllByReportId(report.getId());
+			for (ReportParamRelation relation : relations) {
+				ReportParam reportParam = reportParamRepo.findById(relation.getReportParam().getId()).orElse(null);
+				String key = reportParam.getParamFieldName();
+				String value = null;
+				if (relation.getUseDefault() && !StringUtils.hasLength(reportParam.getParamId())) {
+					value = reportParam.getDefaultValue();
+				} else {
+					value = userParameters.get(reportParam.getParamId());
+				}
+				log.debug("Values sent to report:: key:{}, value:{}", key, value);
+				parameters.put(key, value);
+			}
+			if (!parameters.isEmpty()) {
+				try {
+					return generateReportFromJrxml(jrxmlPath, parameters, fileFormat);
+				} catch (JRException | SQLException | IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return null;
 	}
+
+	public ReportParamDtoList getAllReportFileParameters(String reportId) {
+		Report report = reportRepository.findByReportId(reportId).orElse(null);
+		if (report != null) {
+			List<ReportParamRelation> relations = reportParamRelationRepo.findAllByReportId(report.getId());
+			List<ReportParamDto> reportParams = relations.stream().map(relation -> relation.getReportParam())
+					.map(reportParam -> reportParamMapper.mapReportParam(reportParam)).collect(Collectors.toList());
+
+			return ReportParamDtoList.builder().reportParams(reportParams).build();
+		}
+		return null;
+	}
+
+	public ReportParamDtoList getEnabledReportFileParameters(String reportId) {
+		Report report = reportRepository.findByReportId(reportId).orElse(null);
+		if (report != null) {
+			List<ReportParamRelation> relations = reportParamRelationRepo.findAllByReportId(report.getId());
+			List<ReportParamDto> reportParams = relations.stream().filter(rel -> rel.getEnabled())
+					.map(relation -> relation.getReportParam())
+					.map(reportParam -> reportParamMapper.mapReportParam(reportParam)).collect(Collectors.toList());
+
+			return ReportParamDtoList.builder().reportParams(reportParams).build();
+		}
+		return null;
+	}
+
+	private byte[] generateReportFromJrxml(String jrxmlPath, Map<String, Object> parameters, String format)
+			throws JRException, SQLException, IOException {
+		byte[] reportContent = null;
+		// Load and compile the JRXML
+		// Resource resource = resourceLoader.getResource(jrxmlPath);
+		// JasperReport jasperReport = JasperCompileManager.compileReport(resource.getInputStream());
+		Resource resource = resourceLoader.getResource("classpath:reports/clientReport.jasper");
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(resource.getInputStream());
+
+		// Fill the report
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters,
+				reportDataSource.getConnection());
+
+		// Export to desired format
+		switch (format.toLowerCase()) {
+		case "pdf" -> reportContent = JasperExportManager.exportReportToPdf(jasperPrint);
+		case "xml" -> reportContent = JasperExportManager.exportReportToXml(jasperPrint).getBytes();
+		case "excel" -> reportContent = exportToXls(jasperPrint);
+		case "xls" -> reportContent = exportToXls(jasperPrint);
+		default -> throw new IllegalArgumentException("Unknown report format:" + format);
+		}
+
+		return reportContent;
+	}
+
+	private byte[] exportToXls(JasperPrint jasperPrint) {
+		/*
+		 * try { // Create a ByteArrayOutputStream to hold the Excel data in memory
+		 * ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		 * 
+		 * // Initialize the XLS exporter JRXlsExporter exporter = new JRXlsExporter();
+		 * 
+		 * // Set export parameters exporter.setExporterInput(new
+		 * SimpleExporterInput(jasperPrint)); exporter.setExporterOutput(new
+		 * SimpleOutputStreamExporterOutput(byteArrayOutputStream));
+		 * 
+		 * SimpleXlsReportConfiguration config = new SimpleXlsReportConfiguration();
+		 * config.setOnePagePerSheet(false); config.setDetectCellType(true);
+		 * config.setCollapseRowSpan(false); exporter.setConfiguration(config);
+		 * 
+		 * // Export the report to XLS format exporter.exportReport();
+		 * 
+		 * log.debug("Report exported to XLS successfully!");
+		 * 
+		 * // Convert the ByteArrayOutputStream to a byte array byte[] xlsData =
+		 * byteArrayOutputStream.toByteArray();
+		 * 
+		 * return xlsData;
+		 * 
+		 * } catch (Exception e) { e.printStackTrace(); }
+		 */
+		return null;
+	}
+
 }
