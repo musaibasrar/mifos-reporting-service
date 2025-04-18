6ee93d871c10d3cb6a789f0eeec6c5f3773f01a1
Reehan <reehan.lalkhan@gmail.com>
2025-04-18 15:32:02 +0530

    Removed example code and all its related items

--- src/main/java/org/ideoholic/mrs/jasper/JasperReportService.java
+++ src/main/java/org/ideoholic/mrs/jasper/JasperReportService.java
@@ -1,8 +1,6 @@
 package org.ideoholic.mrs.jasper;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -20,7 +18,6 @@ import org.ideoholic.mrs.dto.ReportGenerationParamsDto;
 import org.ideoholic.mrs.dto.ReportParamDto;
 import org.ideoholic.mrs.dto.ReportParamDtoList;
 import org.ideoholic.mrs.mappers.ReportParamMapper;
-import org.ideoholic.mrs.model.Item;
 import org.ideoholic.mrs.model.Report;
 import org.ideoholic.mrs.model.ReportParam;
 import org.ideoholic.mrs.model.ReportParamRelation;
@@ -29,19 +26,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
-import net.sf.jasperreports.engine.util.JRSaver;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
@@ -69,42 +62,6 @@ public class JasperReportService {
 
 	@Autowired
 	private ReportParamRelationRepository reportParamRelationRepo;
-
-	public byte[] getItemReport(List<Item> items, String format) {
-
-		JasperReport jasperReport;
-
-		try {
-			jasperReport = (JasperReport) JRLoader.loadObject(ResourceUtils.getFile("item-report.jasper"));
-		} catch (FileNotFoundException | JRException e) {
-			try {
-				File file = ResourceUtils.getFile("classpath:item-report.jrxml");
-				jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-				JRSaver.saveObject(jasperReport, "item-report.jasper");
-			} catch (FileNotFoundException | JRException ex) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(items);
-		Map<String, Object> parameters = new HashMap<>();
-		parameters.put("title", "Item Report");
-		JasperPrint jasperPrint = null;
-		byte[] reportContent;
-
-		try {
-			jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-			switch (format) {
-			case "pdf" -> reportContent = JasperExportManager.exportReportToPdf(jasperPrint);
-			case "xml" -> reportContent = JasperExportManager.exportReportToXml(jasperPrint).getBytes();
-			case "xls" -> reportContent = exportToXls(jasperPrint);
-			default -> throw new RuntimeException("Unknown report format");
-			}
-		} catch (JRException e) {
-			throw new RuntimeException(e);
-		}
-		return reportContent;
-	}
 
 	public byte[] generateReport(ReportGenerationParamsDto reportGenerationParamsDto, String reportId,
 			String fileFormat) {
