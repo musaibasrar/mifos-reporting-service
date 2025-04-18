d42817999b90d4ccd531d7b7359b7897648a5143
Reehan <reehan.lalkhan@gmail.com>
2025-04-18 17:56:03 +0530

    Removed example code and all its related items

--- src/main/java/org/ideoholic/mrs/MifosReportingService.java
+++ src/main/java/org/ideoholic/mrs/MifosReportingService.java
@@ -1,9 +1,5 @@
 package org.ideoholic.mrs;
 
-import org.ideoholic.mrs.dao.ItemRepository;
-import org.ideoholic.mrs.model.Item;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
@@ -12,21 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * To access H2 database: http://localhost:8080/h2-console
  */
 @SpringBootApplication
-public class MifosReportingService implements CommandLineRunner {
-
-	@Autowired
-	ItemRepository itemRepository;
+public class MifosReportingService {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MifosReportingService.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		itemRepository.save(new Item("Item 1"));
-		itemRepository.save(new Item("Item 2"));
-		itemRepository.save(new Item("Item 3"));
-		itemRepository.save(new Item("Item 4"));
-		itemRepository.save(new Item("Item 5"));
-	}
 }
--- src/main/java/org/ideoholic/mrs/web/ReportControllerImpl.java
+++ src/main/java/org/ideoholic/mrs/web/ReportControllerImpl.java
@@ -1,9 +1,7 @@
 package org.ideoholic.mrs.web;
 
-import java.io.IOException;
 import java.util.Optional;
 
-import org.ideoholic.mrs.dao.ItemRepository;
 import org.ideoholic.mrs.dto.ReportGenerationParamsDto;
 import org.ideoholic.mrs.dto.ReportParamDtoList;
 import org.ideoholic.mrs.jasper.JasperReportService;
@@ -16,34 +14,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import net.sf.jasperreports.engine.JRException;
 
 @RestController
 public class ReportControllerImpl implements ReportController {
 
 	@Autowired
-	private ItemRepository itemRepository;
-
-	@Autowired
 	private JasperReportService jasperReportService;
-
-	@GetMapping("item-report/{format}")
-	public ResponseEntity<Resource> getItemReport(@PathVariable String format) throws JRException, IOException {
-
-		byte[] reportContent = jasperReportService.getItemReport(itemRepository.findAll(), format);
-
-		ByteArrayResource resource = new ByteArrayResource(reportContent);
-		return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
-				.contentLength(resource.contentLength())
-				.header(HttpHeaders.CONTENT_DISPOSITION,
-						ContentDisposition.attachment().filename("item-report." + format).build().toString())
-				.body(resource);
-	}
 
 	@Override
 	public ResponseEntity<ReportParamDtoList> getAllReportFileParameters(String reportId) {
