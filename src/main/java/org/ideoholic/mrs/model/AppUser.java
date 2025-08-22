4af0af21f4d7f634c99d2b1e262a43e41cb33ae6
Uzma Tazeen <140700588+Uzmatazeen14@users.noreply.github.com>
2025-08-22 16:54:46 +0530

    Created REST API for appUerId parameter

---
+++ src/main/java/org/ideoholic/mrs/model/AppUser.java
@@ -0,0 +1,45 @@
+package org.ideoholic.mrs.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "m_appuser")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "is_self_service_user")
+    private Boolean isSelfServiceUser;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getFirstname() { return firstname; }
+    public void setFirstname(String firstname) { this.firstname = firstname; }
+
+    public String getLastname() { return lastname; }
+    public void setLastname(String lastname) { this.lastname = lastname; }
+
+    public Boolean getIsSelfServiceUser() { return isSelfServiceUser; }
+    public void setIsSelfServiceUser(Boolean isSelfServiceUser) { this.isSelfServiceUser = isSelfServiceUser; }
+}
